58cf512c49c98e3076c564104e3371ceadab8db4
Atsushi Kawamura <akawamura@c-fo.com>
2019-11-21 15:56:51 +0900

    checker

--- go-jira/check.sh
+++ go-jira/check.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
+set -xu
+
 issue_url="https://jira-freee.atlassian.net/browse/"
-slack_url=""
+slack_url="https://hooks.slack.com/services/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"
 
 echo > body.txt
 
 # 未着手なやつ
-issues=$(jira ls -q "project = SRE AND issuetype = Ask AND labels = ask_SRE AND status = \"TO DO\"" | cut -d: -f1)
-
-for issue in issues; do
-  # jira view DONBURI-7290 -t debug | jq '.fields.assignee.name'
-  jira view $issue -t debug | jq -r '[.key = "'${issue_url}'" + .key | .key,.fields.assignee.name] | @csv' >> body.txt
+echo "未着手なチケットです" >> body.txt
+echo "\`\`\`" >> body.txt
+issues=$(jira ls -q "project = SRE AND issuetype = Ask AND labels = ask_SRE AND status = \"TO DO\"" | cut -d: -f1 )
+for issue in $issues; do
+  jira view $issue -t debug | jq -r '[.key = "'${issue_url}'" + .key | .key,.fields.assignee.key,.fields.updated] | @csv' >> body.txt
 done
+echo "\`\`\`" >> body.txt
 
 # 最終更新から1週間以上経っているやつ
+echo "放置されているチケットです" >> body.txt
+echo "\`\`\`" >> body.txt
 issues=$(jira ls -q "project = SRE AND issuetype = Ask AND labels = ask_SRE AND (status = DOING AND updated < \"-1w\")" | cut -d: -f1)
-
-for issue in issues; do
-  jira view $issue -t debug | jq -r '[.key = "'${issue_url}'" + .key | .key,.fields.assignee.name,.fields.updated] | @csv' >> body.txt
+for issue in $issues; do
+  jira view $issue -t debug | jq -r '[.key = "'${issue_url}'" + .key | .key,.fields.assignee.key,.fields.updated] | @csv' >> body.txt
 done
+echo "\`\`\`" >> body.txt
 
-#curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' $slack_url
-curl -X POST -H 'Content-type: application/json' -d @body.json $slack_url
+body=$(cat body.txt | tr '\n' '\\' | sed 's/\\/\\n/g'| sed 's/\"/\\"/g')
+curl -s -S -X POST --data-urlencode "payload={\"text\": \"${body}\" }" ${slack_url}
